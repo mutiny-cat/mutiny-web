a19fec18db6fe299d6dd96e28832c397dbda3712
MUTINYCAT <124721985+mutiny-cat@users.noreply.github.com>
2026-05-30 15:43:21 +0200

    refactor: rename all files to kebab-case

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import icon from 'astro-icon';
 
 export default defineConfig({
   site: 'https://mutiny.cat',
-  integrations: [icon()],
+  integrations: [icon({
+    include: {
+      'simple-icons': ['creativecommons'],
+    },
+  })],
   devToolbar: {
     enabled: false,
   },
